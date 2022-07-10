980d9d083107a4e2c909db78103f0812e52452b9
Kai Murata <kaikaikaim87@gmail.com>
2022-07-10 19:12:51 -0400

    Commit

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 import scrape_mars
 
 # Create an instance of Flask
-app = Flask(__name__)
+app = Flask(__name__) 
 
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
@@ -34,3 +34,4 @@ def scrape():
 
 if __name__ == "__main__":
     app.run(debug=True)
+  
